da9cdddfd00d920314455ec2fa40308b7fa1d295
4Kirishy <kyryloteplynskyi@gmail.com>
2023-04-29 11:02:13 +0000

    implemented specialty model
    - generated specialty model by running rails g model
    - added field name:string to migration file and exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_29_105320) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_110116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_29_105320) do
     t.string "name", null: false
     t.string "location", null: false
     t.date "year", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "specialties", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
