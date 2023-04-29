85ff111ea7ed9dd5da837024747c75c62853362c
4Kirishy <kyryloteplynskyi@gmail.com>
2023-04-29 15:54:11 +0000

    added search logic for hospitals
    - created search query object
    - configured post route for /hospitals in config/routes.rb file
    - styles search bar in views/hospitals/index.html.erb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "hospitals", to: "hospitals#index"    
   get "hospitals/:id", to: "hospitals#detailed"
+  post "hospitals", to: "hospitals#search"
   root "root#index"
 end
