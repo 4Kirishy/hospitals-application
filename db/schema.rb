b99ee38ad3c9ccd1a99bb4cebe3c8d4cf81bbc8c
4Kirishy <kyryloteplynskyi@gmail.com>
2023-04-29 11:28:36 +0000

    implemented patient model
    - gererated patient model by running rails g model
    - added field first_name:string, last_name:string, birth_date:date to migration file
    - migrated patient model migration by running rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_29_111549) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_112616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_29_111549) do
     t.string "name", null: false
     t.string "location", null: false
     t.date "year", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.date "birth_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
