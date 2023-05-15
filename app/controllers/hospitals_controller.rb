d7342a9f7c6e6323403c1395a3a703e3c0b21e58
4Kirishy <kyryloteplynskyi@gmail.com>
2023-05-15 16:34:46 +0000

    added feature to download single hospital info as a pdf file

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,4 +1,6 @@
 class HospitalsController < ApplicationController
+    before_action :set_hospital
+    
     def index
         if not user_signed_in?
             redirect_to root_path
@@ -16,5 +18,26 @@ class HospitalsController < ApplicationController
     def detailed
         @hospital = Hospital.includes(:departments).find(params['id'].to_i)
         render 'detailed'
+    end
+    
+    
+    def pdf
+        pdf = Prawn::Document.new
+        pdf.text @hospital.name, size: 32, style: :bold
+        pdf.move_down 20
+        pdf.text "Location: #{@hospital.city}"
+        pdf.text "Type: #{@hospital.category}"
+        pdf.text "Year: #{@hospital.year}"
+        pdf.move_down 20
+        pdf.text "Departments:", size: 16, style: :bold
+        @hospital.departments.each do |dep|
+            pdf.text "- #{dep.name}"
+        end
+        send_data(pdf.render, filename: "hospitals.pdf", type: "application/pdf", disposition: "inline")
+    end
+    
+    
+    def set_hospital
+        @hospital = Hospital.includes(:departments).find(params['id'].to_i)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get "hospitals", to: "hospitals#index"    
   get "hospitals/:id", to: "hospitals#detailed"
   post "hospitals", to: "hospitals#search"
+  
+  get 'download/hospitals/pdf/:id', to: "hospitals#pdf", as: "hospitals_pdf"
+  
   root "root#index"
 end
